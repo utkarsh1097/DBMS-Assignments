10dcf35d0b9cd0c0e5c622399cac3c7327c36c41
Utkarsh Singh <utkarshsinghoct1997@gmail.com>
2018-06-30 16:48:06 +0530

    Update part_a.sh

--- Assignment 3/part_a.sh
+++ Assignment 3/part_a.sh
@@ -1,7 +1,7 @@
-echo "Running 20NN queries"
+echo -n "Running 20NN queries"
 for i in 1 2 3 4 
 do
-	sleep 1
+	sleep 0.3
 	echo -n "."
 done
 sleep 1
@@ -10,23 +10,26 @@ sleep 1
 echo -n "Doing sequential scan"
 for i in 1 2 3 4 
 do
-	sleep 1
+	sleep 0.3
 	echo -n "."
 done
 sleep 1
 echo "."
-g++ -std=c++14 seqscan.cpp
+g++ -std=c++14 seqscan.cpp -no-pie
 ./a.out
 echo -n "Doing best first search"
 for i in 1 2 3 4 
 do
-	sleep 1
+	sleep 0.3
 	echo -n "."
 done
 sleep 1
 echo "."
-g++ -std=c++14 bestfirstsearch.cpp
+g++ -std=c++14 bestfirstsearch.cpp -no-pie
 ./a.out
 echo "Plotting the graph!"
 sleep 2
 python3 plot_graph_times.py
+rm a.out
+rm seqscan_time.txt
+rm bestfirstsearch_time.txt
