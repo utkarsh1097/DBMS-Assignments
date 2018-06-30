dd5cc3eafe5c40c03a1eb427cdc03231ca457ab7
Utkarsh Singh <utkarshsinghoct1997@gmail.com>
2018-06-30 16:52:44 +0530

    Update data_query.sh

--- Assignment 3/data_query.sh
+++ Assignment 3/data_query.sh
@@ -1,18 +1,20 @@
 echo -n "Generating dataset"
 for i in 1 2 3 4 
 do
-	sleep 1
+	sleep 0.3
 	echo -n "."
 done
 echo "."
-g++ -std=c++14 generate_dataset.cpp
-./a.out
+g++ -std=c++14 -o generate_dataset generate_dataset.cpp -no-pie
+./generate_dataset
+rm generate_dataset
 echo -n "Generating queryset"
 for i in 1 2 3 4 
 do
-	sleep 1
+	sleep 0.3
 	echo -n "."
 done
 echo "."
-g++ -std=c++14 generate_queryset.cpp
-./a.out
+g++ -std=c++14 -o generate_queryset generate_queryset.cpp -no-pie
+./generate_queryset
+rm generate_queryset
